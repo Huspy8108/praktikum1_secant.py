ccb826320057ef6103f4a975762aabbeda4ec9e7
Huspy8108 <aqsanmuhammad808@gmail.com>
2026-05-01 15:44:11 +0700

    Update praktikum1_secant.py

--- praktikum1_secant.py
+++ praktikum1_secant.py
@@ -1,7 +1,13 @@
 import math
+import re
 
-# Fungsi untuk membaca input fungsi dari user
+
+
+# Fungsi untuk membaca input 
 def buat_fungsi(expr):
+    expr = expr.replace("^", "**")                      
+    expr = re.sub(r'(\d)([a-zA-Z(])', r'\1*\2', expr)
+    expr = re.sub(r'(\))(\w)', r'\1*\2', expr)          
     def f(x):
         return eval(expr, {"__builtins__": None}, {
             "x": x,
@@ -16,16 +22,13 @@ def buat_fungsi(expr):
         })
     return f
 
-
-print("=" * 80)
 print("PROGRAM PENCARIAN AKAR PERSAMAAN - METODE SECANT")
-print("=" * 80)
 
-print("Contoh fungsi:")
+print("Contoh penulisan fungsi:")
+print("Pangkat Terbesar wajib didepan")
 print("  x**3 - x - 2")
 print("  cos(x) - x")
 print("  exp(-x) - x")
-print("-" * 80)
 
 # Input user
 fungsi_input = input("Masukkan fungsi f(x): ")
@@ -42,22 +45,21 @@ print(f"{'Iterasi':<10}{'x0':<15}{'x1':<15}{'x2':<15}{'f(x2)':<15}{'Error':<15}"
 print("=" * 95)
 
 akar = None
-
-for i in range(1, maks_iterasi + 1):
+#loop iterasi
+for i in range(1,maks_iterasi+1):
     fx0 = f(x0)
     fx1 = f(x1)
 
-    # Hindari pembagian nol
-    if fx1 - fx0 == 0:
-        print("Terjadi pembagian dengan nol. Proses dihentikan.")
+    #stop saat dibagi 0
+    if fx1-fx0==0:
+        print("Terjadi pembagian dengan nol.")
         break
 
     # Rumus Secant
-    x2 = x1 - (fx1 * (x1 - x0)) / (fx1 - fx0)
+    x2 = x1-(fx1*(x1 - x0))/(fx1-fx0)
     fx2 = f(x2)
 
     error = abs(x2 - x1)
-
     print(f"{i:<10}{x0:<15.6f}{x1:<15.6f}{x2:<15.6f}{fx2:<15.6f}{error:<15.6f}")
 
     # Cek konvergensi
@@ -72,7 +74,7 @@ for i in range(1, maks_iterasi + 1):
 print("=" * 95)
 
 if akar is not None:
-    print(f"[✓] Akar ditemukan: x = {akar:.6f}")
-    print(f"[✓] Nilai f(x): {f(akar):.6f}")
+    print(f"nilai akar = {akar:.6f}")
+    print(f"nilai f(x): {f(akar):.6f}")
 else:
     print("[!] Akar belum ditemukan.")
